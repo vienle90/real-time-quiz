110c2cbaecb02e1dbde5ef3e822e9422bbcd33e2
Vien Le <vien.sts@tamara.co>
2025-04-26 21:06:21 +0700

    fix all errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,17 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\LeaderboardRepositoryInterface;
 use App\Repositories\Contracts\QuestionChoiceRepositoryInterface;
+use App\Repositories\Contracts\QuestionRepositoryInterface;
 use App\Repositories\Contracts\QuizRepositoryInterface;
 use App\Repositories\Contracts\QuizUserRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\QuestionChoiceRepository;
+use App\Repositories\QuestionRepository;
 use App\Repositories\QuizRepository;
 use App\Repositories\QuizUserRepository;
+use App\Repositories\RedisRepositorires\RedisLeaderBoardRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
@@ -25,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(QuizUserRepositoryInterface::class, QuizUserRepository::class);
         $this->app->bind(QuestionChoiceRepositoryInterface::class, QuestionChoiceRepository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(LeaderboardRepositoryInterface::class, RedisLeaderBoardRepository::class);
+        $this->app->bind(QuestionRepositoryInterface::class, QuestionRepository::class);
     }
 
     /**
