abf7ef4daf11e29f08a7ade2d2012b77110b6aff
Vien Le <vien.sts@tamara.co>
2025-04-26 15:37:54 +0700

    fix provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,44 +2,29 @@
 
 namespace App\Providers;
 
-use App\Listeners\CreateRedisUser;
-use App\Repositories\Contracts\LeaderboardRepositoryInterface;
 use App\Repositories\Contracts\QuestionChoiceRepositoryInterface;
-use App\Repositories\Contracts\QuestionRepositoryInterface;
 use App\Repositories\Contracts\QuizRepositoryInterface;
 use App\Repositories\Contracts\QuizUserRepositoryInterface;
-use App\Repositories\Contracts\UserQuestionAnswerRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\QuestionChoiceRepository;
-use App\Repositories\QuestionRepository;
 use App\Repositories\QuizRepository;
 use App\Repositories\QuizUserRepository;
-use App\Repositories\RedisRepositorires\RedisLeaderBoardRepository;
-use App\Repositories\RedisRepositorires\RedisUserRepository;
-use App\Repositories\UserQuestionAnswerRepository;
 use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public $bindings = [
-        LeaderboardRepositoryInterface::class => RedisLeaderBoardRepository::class,
-        QuizRepositoryInterface::class => QuizRepository::class,
-        UserRepositoryInterface::class => UserRepository::class,
-        QuizUserRepositoryInterface::class => QuizUserRepository::class,
-        QuestionRepositoryInterface::class => QuestionRepository::class,
-        QuestionChoiceRepositoryInterface::class => QuestionChoiceRepository::class,
-        UserQuestionAnswerRepositoryInterface::class => UserQuestionAnswerRepository::class,
-    ];
-
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        $this->app->when(CreateRedisUser::class)
-            ->needs(UserRepositoryInterface::class)
-            ->give(RedisUserRepository::class);
+        // Register repository bindings
+        $this->app->bind(QuizRepositoryInterface::class, QuizRepository::class);
+        $this->app->bind(QuizUserRepositoryInterface::class, QuizUserRepository::class);
+        $this->app->bind(QuestionChoiceRepositoryInterface::class, QuestionChoiceRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
     }
 
     /**
@@ -47,6 +32,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Vite::prefetch(concurrency: 3);
     }
 }
